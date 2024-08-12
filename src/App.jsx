114975cffe41991771e01ea9f573d54154ab946d
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-08-12 18:02:50 +0100

    commmit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
 import { useEffect, useState } from "react"
 import { db } from "./firebaseConfig"
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, getDocs, addDoc, doc } from 'firebase/firestore'
 
 function App() {
   const [users, setUsers] = useState([])
-  const [newAge, setNewAge] = useState('')
+  const [newAge, setNewAge] = useState(0)
   const [newName, setNewName] = useState('')
+  const [surname, setSurname] = useState('')
 
   const usersCollection = collection(db, "users")
 
-  const handleCreateUser = async () => {
+  const handleAgeIncrease = async (id, age) => {
+    const userDoc = doc(db, "users", id)
+    const upDatedAge = {age: age + 1}
+    const updateDoc = (userDoc, upDatedAge)
+  }
 
+  const handleCreateUser = async (e) => {
+    e.preventDefault()
+    try{
+      await addDoc(usersCollection, {name: newName, age: Number(newAge), surname: surname})
+      alert('successful')
+      setNewAge(0)
+      setNewName('')
+      setSurname('')
+    } catch (error) {
+      alert('connection failed')
+    }
   }
 
   useEffect(() => {
@@ -20,24 +36,26 @@ function App() {
     }
 
     getUsers()
-  }, [])
+  }, [usersCollection, db])
 
   return (
    <main className=" container">
       <ul className=" space-y-4">
         {
           users.map((user) => (
-            <li className=" text-center">
+            <li key={user.id} className=" text-center">
               <p className=" text-2xl font-bold">Name: {user.name}</p>
               <p className=" text-2xl font-bold">Age {user.age}</p>
+              <button onClick={() => handleAgeIncrease(user.id, user.age)} className=" btn bg-green-500">Increase Age</button>
             </li>
           ))
         }
       </ul>
       <div>
         <form onSubmit={handleCreateUser} className=" space-y-2">
-          <input className=" border p-2 rounded-lg" type="text" placeholder="Enter name" onChange={(e) => setName(e.target.value)} />
-          <input className=" border p-2 rounded-lg" type="number" placeholder="Enter age" on/>
+          <input className=" border p-2 rounded-lg" type="text" placeholder="Enter name" value={newName} onChange={(e) => setNewName(e.target.value)} />
+          <input className=" border p-2 rounded-lg" type="number" placeholder="Enter age" value={newAge} onChange={(e) => setNewAge(e.target.value)}/>
+          <input className=" border p-2 rounded-lg" type="text" placeholder="Enter surname" value={surname} onChange={(e) => setSurname(e.target.value)}/>
           <div>
             <button className=" btn" type="submit">Create User</button>
           </div>
